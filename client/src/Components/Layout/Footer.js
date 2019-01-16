82f4986688b0f16e4a3c94a81eb82aec18ab1f49
treyham91 <treyhamilton23@outlook.com>
2019-01-15 20:55:52 -0600

    Initial React commit

--- client/src/Components/Layout/Footer.js
+++ client/src/Components/Layout/Footer.js
@@ -4,6 +4,7 @@ export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} CMS Pro
+        <p>This is a test change for the branch.</p>
     </footer>
   );
 };
